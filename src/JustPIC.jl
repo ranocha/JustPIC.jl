53bb2dc4e49d68ae24bc0c0006965bb898a0876b
albert-de-montserrat <albert.de.montserrat@gmail.com>
2022-10-11 10:30:44 +0200

    fix CUDA conditional loading

--- src/JustPIC.jl
+++ src/JustPIC.jl
@@ -1,30 +1,21 @@
 module JustPIC
 
 using MuladdMacro
-# using CUDA
 using ParallelStencil
-
-# include(
-#     "C:\\Users\\albert\\Desktop\\StencilInterpolations.jl\\src\\StencilInterpolations.jl"
-# )
-# using .StencilInterpolations
-
-# import .StencilInterpolations: _grid2particle, parent_cell, isinside
-# export grid2particle!, gathering!, grid2particle_xcell!, gathering_xcell!
-
 using StencilInterpolations
 
-export gathering!, gathering_xcell!, gathering_xvertex!
-export grid2particle!, grid2particle_xvertex!, grid2particle_xcell!
+const PS_PACKAGE = ENV["PS_PACKAGE"]
 
-const PS_PACKAGE = Symbol(ENV["PS_PACKAGE"])
+eval(:(@static PS_PACKAGE === "CUDA" && using CUDA))
 
-!ParallelStencil.is_initialized() && eval(:(@static if PS_PACKAGE == :CUDA
+!ParallelStencil.is_initialized() && eval(:(@static if PS_PACKAGE === "CUDA"
     @init_parallel_stencil(CUDA, Float64, 2)
-    CUDA.allowscalar(true)
 elseif PS_PACKAGE == :Threads
     @init_parallel_stencil(Threads, Float64, 2)
 end))
+
+export gathering!, gathering_xcell!, gathering_xvertex!
+export grid2particle!, grid2particle_xvertex!, grid2particle_xcell!
 
 include("particles.jl")
 export Particles, init_particles, particle2grid!
